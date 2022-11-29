af1905975771d14e07a94e8021633b1fd3dc7238
guanym <1195817329@qq.com>
2022-11-29 21:56:28 +0800

    feat: 增加模块选择功能

--- src/create.js
+++ src/create.js
@@ -1,8 +1,8 @@
 /*
  * @Author: guanyaoming guanyaoming@linklogis.com
  * @Date: 2022-11-10 19:48:01
- * @LastEditors: guanyaoming guanyaoming@linklogis.com
- * @LastEditTime: 2022-11-11 14:53:02
+ * @LastEditors: guanym 1195817329@qq.com
+ * @LastEditTime: 2022-11-29 20:49:34
  * @FilePath: \energise-cli\src\create.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,7 +28,7 @@ async function main() {
       choices: templateRepoNames,
     },
   ]);
-  tagetTempUrl = templateRepos[answers.tagetTempName];
+  tagetTempUrl = templateRepos[answers.tagetTempName][url];
 
   inquirer
     .prompt([
