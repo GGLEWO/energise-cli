bfad9ad32ecc19de68892a5b141e0e44c4b5d27b
guanyaoming <guanyaoming@linklogis.com>
2022-12-02 20:35:34 +0800

    feat: 路由改造

--- src/create.js
+++ src/create.js
@@ -1,25 +1,66 @@
 /*
  * @Author: guanyaoming guanyaoming@linklogis.com
  * @Date: 2022-11-10 19:48:01
- * @LastEditors: guanym 1195817329@qq.com
- * @LastEditTime: 2022-11-29 20:49:34
+ * @LastEditors: guanyaoming guanyaoming@linklogis.com
+ * @LastEditTime: 2022-12-02 20:18:40
  * @FilePath: \energise-cli\src\create.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 const templateRepos = require("../templates.json");
+const utils = require("./utils");
 const download = require("download-git-repo");
 const inquirer = require("inquirer");
 const ora = require("ora");
 const chalk = require("chalk");
-
 const fs = require("fs");
 const path = require("path");
 const Handlebars = require("handlebars");
 
+const handleInitProject = function (answers, targetTemp) {
+  // 处理 package.json 文件
+  const packagePath = path.join(
+    path.resolve("./"),
+    `/${answers.name}/package.json`
+  );
+  const content = JSON.stringify(require(packagePath), "", "\t");
+  const template = Handlebars.compile(content);
+  const result = template(answers);
+  fs.writeFileSync(packagePath, result);
+
+  const choicedMoudleLabel = answers.moduleArr.map((e) => e?.label);
+
+  // 处理路由文件
+  const routerPath = path.join(
+    path.resolve("./"),
+    `/${answers.name}/src/router/routes.js`
+  );
+  let routerContent = require(routerPath);
+  const optionsRoutes = routerContent.optionsRoutes
+    .map((e) => {
+      if (choicedMoudleLabel.includes(e.name)) return e;
+    })
+    .filter((v) => v);
+  let routeRes = `exports.optionsRoutes = ${JSON.stringify(optionsRoutes)}`;
+  fs.writeFileSync(routerPath, routeRes);
+
+  // 处理 views 文件夹
+  // const targetPath = path.join(path.resolve("./"), `/${answers.name}`);
+  // const targetTempModule = targetTemp.moduleArr;
+  // /**
+  //  * @description: 需要删除的目录
+  //  * @return {*}
+  //  */
+  // let needDelDirList = targetTempModule.filter(
+  //   (e) => !choicedMoudleLabel.includes(e.value.label)
+  // );
+  // needDelDirList.forEach((val) => {
+  //   utils.removeDir(`${targetPath}${val.value.filePath}`);
+  // });
+};
+
 async function main() {
   let templateRepoNames = Object.keys(templateRepos);
   let tagetTempUrl = null;
-
   let answers = await inquirer.prompt([
     {
       type: "list",
@@ -28,8 +69,7 @@ async function main() {
       choices: templateRepoNames,
     },
   ]);
-  tagetTempUrl = templateRepos[answers.tagetTempName][url];
-
+  const targetTemp = templateRepos[answers.tagetTempName];
   inquirer
     .prompt([
       {
@@ -44,12 +84,19 @@ async function main() {
       },
       { name: "description", message: "请输入项目描述" },
       { name: "author", message: "请输入作者" },
+      {
+        name: "moduleArr",
+        message: "请选择需要的菜单模块",
+        type: "checkbox",
+        choices: targetTemp.moduleArr,
+      },
     ])
     .then((answers) => {
+      console.log(answers, "answers");
       const spinner = ora("项目模板下载中...");
       spinner.start();
       download(
-        `direct:${tagetTempUrl}`,
+        `direct:${targetTemp.url}`,
         answers.name,
         { clone: true },
         (err) => {
@@ -59,14 +106,7 @@ async function main() {
             return;
           }
           spinner.succeed();
-          const packagePath = path.join(
-            path.resolve("./"),
-            `/${answers.name}/package.json`
-          );
-          const content = JSON.stringify(require(packagePath), "", "\t");
-          const template = Handlebars.compile(content);
-          const result = template(answers);
-          fs.writeFileSync(packagePath, result);
+          handleInitProject(answers, targetTemp);
         }
       );
     });
---
+++ src/utils.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: guanyaoming guanyaoming@linklogis.com
+ * @Date: 2022-11-30 16:04:29
+ * @LastEditors: guanyaoming guanyaoming@linklogis.com
+ * @LastEditTime: 2022-11-30 16:19:30
+ * @FilePath: \energise-cli\src\utils.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+const fs = require("fs");
+const path = require("path");
+const removeDir = function (dir) {
+  let files = fs.readdirSync(dir);
+  for (var i = 0; i < files.length; i++) {
+    let newPath = path.join(dir, files[i]);
+    let stat = fs.statSync(newPath);
+    if (stat.isDirectory()) {
+      //如果是文件夹就递归下去
+      removeDir(newPath);
+    } else {
+      //删除文件
+      fs.unlinkSync(newPath);
+    }
+  }
+  fs.rmdirSync(dir); //如果文件夹是空的，就将自己删除掉
+};
+
+module.exports.removeDir = removeDir;
