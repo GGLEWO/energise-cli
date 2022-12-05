a790f4d38a932db9f056e2e3168315be0a0a5adf
guanyaoming <guanyaoming@linklogis.com>
2022-12-05 20:02:13 +0800

    fix: 替换 fs-extra

--- src/create.js
+++ src/create.js
@@ -2,7 +2,7 @@
  * @Author: guanyaoming guanyaoming@linklogis.com
  * @Date: 2022-11-10 19:48:01
  * @LastEditors: guanyaoming guanyaoming@linklogis.com
- * @LastEditTime: 2022-12-05 17:06:10
+ * @LastEditTime: 2022-12-05 20:01:42
  * @FilePath: \energise-cli\src\create.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,7 +14,7 @@ const ora = require("ora");
 const chalk = require("chalk");
 const path = require("path");
 const Handlebars = require("handlebars");
-const fse = require("fs-extra");
+const fse = require("fs");
 
 const handleInitProject = function (answers, targetTemp) {
   // 处理 package.json 文件
