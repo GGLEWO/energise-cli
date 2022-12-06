487fd6d0b6259151c3552f586d66ab8891d5b9f7
guanyaoming <guanyaoming@linklogis.com>
2022-12-06 11:31:53 +0800

    update: www.js

--- bin/www.js
+++ bin/www.js
@@ -1,11 +1,3 @@
-/*
- * @Author: guanyaoming guanyaoming@linklogis.com
- * @Date: 2022-11-10 17:38:25
- * @LastEditors: guanyaoming guanyaoming@linklogis.com
- * @LastEditTime: 2022-12-06 11:00:13
- * @FilePath: \energise-cli\bin\www.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 #!/usr/bin/env node
 const program = require("commander");
 const version = require("../package.json").version;
