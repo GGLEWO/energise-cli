63c9d6eb4d4d485290060cde4de3d95713926274
guanyaoming <guanyaoming@linklogis.com>
2024-01-11 14:37:09 +0800

    init

--- src/create.js
+++ src/create.js
@@ -1,13 +1,7 @@
-/*
- * @Author: guanyaoming
- * @Date: 2022-11-10 19:48:01
- * @LastEditors: guanyaoming
- * @LastEditTime: 2022-12-05 20:19:53
- * @FilePath: \energise-cli\src\create.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+const esprima = require("esprima");
+const estraverse = require("estraverse");
 const templateRepos = require("../templates.json");
-const utils = require("./utils");
+const { downloadFolder, downloadFile } = require("./utils");
 const download = require("download-git-repo");
 const inquirer = require("inquirer");
 const ora = require("ora");
@@ -15,116 +9,205 @@ const chalk = require("chalk");
 const path = require("path");
 const Handlebars = require("handlebars");
 const fse = require("fs");
-
-const handleInitProject = function (answers, targetTemp) {
-  // 处理 package.json 文件
-  const packagePath = path.join(
-    path.resolve("./"),
-    `/${answers.name}/package.json`
-  );
-  const content = JSON.stringify(require(packagePath), "", "\t");
-  const template = Handlebars.compile(content);
-  const result = template(answers);
-  fse.writeFile(packagePath, result, (err) => {
-    if (err) {
-      return console.error(err);
-    }
-  });
-
-  const choicedMoudleLabel = answers.moduleArr.map((e) => e?.label);
-
-  // 处理路由文件
-  const routerPath = path.join(
-    path.resolve("./"),
-    `/${answers.name}/src/router/routes.js`
-  );
-  let routerContent = require(routerPath);
-  const optionsRoutes = routerContent.optionsRoutes
-    .map((e) => {
-      if (choicedMoudleLabel.includes(e.name)) return e;
-    })
-    .filter((v) => v);
-  fse.writeFile(
-    routerPath,
-    `exports.optionsRoutes = ${JSON.stringify(optionsRoutes, "", "\t")}`,
-    (err) => {
-      if (err) {
-        return console.error(err);
-      }
-    }
-  );
-
-  // 处理 views 文件夹
-  const targetPath = path.join(path.resolve("./"), `/${answers.name}`);
-  const targetTempModule = targetTemp.moduleArr;
-  /**
-   * @description: 需要删除的目录
-   * @return {*}
-   */
-  let needDelDirList = targetTempModule.filter(
-    (e) => !choicedMoudleLabel.includes(e.value.label)
-  );
-  needDelDirList.forEach((val) => {
-    utils.removeDir(`${targetPath}${val.value.filePath}`);
-  });
-};
+const axios = require("axios");
 
 async function main() {
   let templateRepoNames = Object.keys(templateRepos);
-  let tagetTempUrl = null;
   let answers = await inquirer.prompt([
     {
       type: "list",
       name: "tagetTempName",
-      message: "请选择仓库模板",
+      message: "请选择模板类型",
       choices: templateRepoNames,
     },
   ]);
-  const targetTemp = templateRepos[answers.tagetTempName];
-  inquirer
-    .prompt([
-      {
-        name: "name",
-        message: "请输入项目名称",
-        validate: function (val) {
-          if (!/^[a-z0-9-~][a-z0-9-._~]*$/.test(val)) {
-            return "/^[a-z0-9-~][a-z0-9-._~]*$/ 请按该规则创建项目名称";
-          }
-          if (val) {
-            return true;
-          }
+  const typeContent = templateRepos[answers.tagetTempName];
+  switch (answers.tagetTempName) {
+    case "project":
+      handleProjectCase(typeContent);
+      break;
 
-          return "请输入项目名称";
-        },
-      },
-      { name: "description", message: "请输入项目描述" },
-      { name: "author", message: "请输入作者" },
-      {
-        name: "moduleArr",
-        message: "请选择需要的菜单模块",
-        type: "checkbox",
-        choices: targetTemp.moduleArr,
-      },
-    ])
-    .then((answers) => {
-      console.log(answers, "answers");
-      const spinner = ora("项目模板下载中...");
-      spinner.start();
-      download(
-        `direct:${targetTemp.url}#master-tem`,
-        answers.name,
-        { clone: true },
-        (err) => {
-          if (err) {
-            chalk.red(`项目生成失败：${err}`);
-            spinner.fail();
-            return;
-          }
-          spinner.succeed();
-          handleInitProject(answers, targetTemp);
-        }
+    case "module":
+      handleModuleCase(typeContent);
+      break;
+    case "snippet":
+      handleSnippetCase(typeContent);
+  }
+
+  function handleProjectCase(typeContent) {
+    const handleInitProject = function (answers) {
+      // 处理 package.json 文件，更换模板内容
+      const packagePath = path.join(
+        path.resolve("./"),
+        `/${answers.name}/package.json`
       );
-    });
+      const content = JSON.stringify(require(packagePath), "", "\t");
+      const template = Handlebars.compile(content);
+      const result = template(answers);
+      fse.writeFile(packagePath, result, (err) => {
+        if (err) {
+          return console.error(err);
+        }
+      });
+    };
+    inquirer
+      .prompt([
+        {
+          name: "name",
+          message: "请输入项目名称",
+          validate: function (val) {
+            if (!/^[a-z0-9-~][a-z0-9-._~]*$/.test(val)) {
+              return "/^[a-z0-9-~][a-z0-9-._~]*$/ 请按该规则创建项目名称";
+            }
+            if (val) {
+              return true;
+            }
+            return "请输入项目名称";
+          },
+        },
+        { name: "description", message: "请输入项目描述" },
+        { name: "author", message: "请输入作者" },
+        {
+          name: "option",
+          message: "请选择项目模板",
+          type: "list",
+          choices: typeContent.option,
+        },
+      ])
+      .then((answers) => {
+        console.log(answers, answers.option, "answers");
+        const spinner = ora("项目模板下载中...");
+        spinner.start();
+        download(
+          `direct:${answers.option}#master`,
+          answers.name,
+          { clone: true },
+          (err) => {
+            if (err) {
+              chalk.red(`项目生成失败：${err}`);
+              spinner.fail();
+              return;
+            }
+            spinner.succeed();
+            handleInitProject(answers, typeContent);
+          }
+        );
+      });
+  }
+
+  async function handleModuleCase(typeContent) {
+    inquirer
+      .prompt([
+        {
+          name: "option",
+          message: "请选择引入模块",
+          type: "list",
+          choices: typeContent.option,
+        },
+        { name: "outputPath", message: "请输入目标路径" },
+      ])
+      .then(async (answers) => {
+        console.log(answers, answers.option, "answers");
+        const spinner = ora("模块文件下载中...");
+        spinner.start();
+        try {
+          await downloadFolder(
+            answers.option[0],
+            answers.option[1],
+            answers.outputPath
+          );
+          spinner.succeed();
+        } catch (e) {
+          chalk.red(`项目生成失败：${err}`);
+          spinner.fail();
+        }
+      });
+  }
+
+  async function handleModuleCase(typeContent) {
+    inquirer
+      .prompt([
+        {
+          name: "option",
+          message: "请选择引入模块",
+          type: "list",
+          choices: typeContent.option,
+        },
+        { name: "outputPath", message: "请输入目标路径" },
+      ])
+      .then(async (answers) => {
+        console.log(answers, answers.option, "answers");
+        const spinner = ora("模块文件下载中...");
+        spinner.start();
+        try {
+          await downloadFolder(
+            answers.option[0],
+            answers.option[1],
+            answers.outputPath
+          );
+          spinner.succeed();
+        } catch (e) {
+          chalk.red(`模块文件下载失败：${err}`);
+          spinner.fail();
+        }
+      });
+  }
+
+  async function handleSnippetCase(typeContent) {
+    inquirer
+      .prompt([
+        {
+          name: "option",
+          message: "请选择代码片段",
+          type: "list",
+          choices: typeContent.option,
+        },
+      ])
+      .then(async (answers) => {
+        console.log(answers, answers.option, "answers");
+        const spinner = ora("代码片段下载中...");
+        spinner.start();
+        try {
+          let response = await axios.get(
+            `https://api.github.com/repos/${answers.option[0]}/contents/${answers.option[1]}`
+          );
+          let downContent = await downloadFile(response.data.download_url);
+          let fileContent = Buffer.from(downContent).toString("utf-8");
+          let resultContent = null;
+          console.log(
+            fileContent,
+            answers.option[2].functionName,
+            "fileContent"
+          );
+          // 解析代码
+          const ast = esprima.parseModule(fileContent, { range: true });
+          estraverse.traverse(ast, {
+            enter: function (node) {
+              if (
+                node.type === "FunctionDeclaration" &&
+                node.id.name === answers.option[2].functionName
+              ) {
+                resultContent = fileContent.substring(
+                  node.range[0],
+                  node.range[1]
+                );
+                return estraverse.VisitorOption.Break; // 找到后中断遍历
+              }
+            },
+          });
+          console.log(resultContent, "resultContent 结果");
+          (async () => {
+            const clipboardy = await import('clipboardy');
+            clipboardy.default.writeSync(resultContent);
+          })();
+          chalk.green(`${answers.option[2].functionName} 函数已经复制到剪切板`);
+          spinner.succeed();
+        } catch (e) {
+          chalk.red(`代码片段下载失败：${e.message}`);
+          spinner.fail();
+        }
+      });
+  }
 }
 
 main();
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,14 @@
 /*
  * @Author: guanyaoming
  * @Date: 2022-11-30 16:04:29
- * @LastEditors: guanyaoming
- * @LastEditTime: 2022-11-30 16:19:30
+ * @LastEditors: guanyaoming guanyaoming@linklogis.com
+ * @LastEditTime: 2024-01-10 20:11:07
  * @FilePath: \energise-cli\src\utils.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
+const axios = require("axios");
+const fsExtra = require("fs-extra");
 const fs = require("fs");
 const path = require("path");
 const removeDir = function (dir) {
@@ -25,4 +27,38 @@ const removeDir = function (dir) {
   fs.rmdirSync(dir); //如果文件夹是空的，就将自己删除掉
 };
 
-module.exports.removeDir = removeDir;
+async function downloadFile(fileUrl, outputPath) {
+  try {
+    const response = await axios.get(fileUrl, { responseType: "arraybuffer" });
+    console.log("请求成功", outputPath);
+    if (outputPath) {
+      await fsExtra.outputFile(outputPath, response.data);
+    }
+    return response.data;
+  } catch (e) {
+    console.error(`请求失败！ ${fileUrl} + ${e.message}`);
+    return `文件下载失败 + ${fileUrl} + ${e.message}`;
+  }
+}
+
+async function downloadFolder(repo, folderPath, localPath) {
+  const apiUrl = `https://api.github.com/repos/${repo}/contents/${folderPath}`;
+  const response = await axios.get(apiUrl);
+  const files = response.data;
+
+  for (const file of files) {
+    const filePath = path.join(localPath, file.path.replace(folderPath, ""));
+    if (file.type === "file") {
+      await downloadFile(file.download_url, filePath);
+    } else if (file.type === "dir") {
+      await downloadFolder(repo, file.path, filePath);
+    }
+  }
+  return true;
+}
+
+module.exports = {
+  removeDir,
+  downloadFolder,
+  downloadFile,
+};
